1252c096572ebebc2fbff2442e0b7b71bbed795e
Robert Lord <robert@lord.io>
2016-11-28 15:57:38 -0800

    Add simple text token separation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
 extern crate llvm;
+#[macro_use]
+extern crate nom;
+
+enum Token {
+  Eof,
+  Def,
+  Extern,
+  Identifier(String),
+  Number(f64)
+}
+
+fn token_char(chr: char) -> bool {
+  chr.is_alphanumeric() || chr == '.'
+}
+
+named!(white_space<&str, &str>, alt!(tag_s!(" ") | tag_s!("\t") | tag_s!("\n")));
+
+named!(token<&str, &str>, take_while_s!(token_char));
+
+// named!(comment<&str, &str>, alt!())
+
+named!(lex< &str, Vec<&str> >, many0!(delimited!(
+  many0!(white_space),
+  token,
+  many0!(white_space)
+)));
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", lex(&"hello    \ntesting blah"));
 }
