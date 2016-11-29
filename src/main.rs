b47571e6f3f0e92e48ffd232f1cacc9a9f3bc8f2
Robert Lord <robert@lord.io>
2016-11-28 16:02:18 -0800

    Add keywords and floats

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate llvm;
 #[macro_use]
 extern crate nom;
 
+#[derive(Debug)]
 enum Token {
   Eof,
   Def,
@@ -16,16 +17,31 @@ fn token_char(chr: char) -> bool {
 
 named!(white_space<&str, &str>, alt!(tag_s!(" ") | tag_s!("\t") | tag_s!("\n")));
 
-named!(token<&str, &str>, take_while_s!(token_char));
+named!(token<&str, Token>, chain!(
+  tok: take_while_s!(token_char) ,
+  || {
+    if tok == "def" {
+      Token::Def
+    } else if tok == "extern" {
+      Token::Extern
+    } else if let Ok(n) = tok.parse::<f64>() {
+      Token::Number(n)
+    } else {
+      Token::Identifier(tok.to_string())
+    }
+  }
+));
+
+// take_while_s!(token_char));
 
 // named!(comment<&str, &str>, alt!())
 
-named!(lex< &str, Vec<&str> >, many0!(delimited!(
+named!(lex< &str, Vec<Token> >, many0!(delimited!(
   many0!(white_space),
   token,
   many0!(white_space)
 )));
 
 fn main() {
-    println!("{:?}", lex(&"hello    \ntesting blah"));
+    println!("{:?}", lex(&"hello    \ntesting def extern  129382 1.198 198.2e2 blah"));
 }
