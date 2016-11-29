0df543d5dd6adbff82c616bd59f0a133e5498fc2
Robert Lord <robert@lord.io>
2016-11-28 16:46:28 -0800

    Move lexer into lexer module

---
+++ src/lexer.rs
@@ -0,0 +1,50 @@
+#[derive(Debug)]
+pub enum Token {
+  Def,
+  Extern,
+  Identifier(String),
+  Symbol(char),
+  Number(f64)
+}
+
+fn token_char(chr: char) -> bool {
+  chr.is_alphanumeric() || chr == '.'
+}
+
+fn comment_char(chr: char) -> bool {
+  chr != '\n'
+}
+
+named!(comment<&str, &str>, preceded!(
+  tag_s!("#"),
+  take_while_s!(comment_char)
+));
+
+named!(white_space<&str, &str>, alt!(comment | tag_s!(" ") | tag_s!("\t") | tag_s!("\n")));
+
+named!(token<&str, Token>, chain!(
+  tok: alt!(take_while1_s!(token_char) | take_s!(1)) ,
+  || {
+    println!("{:?}", tok);
+    if tok == "def" {
+      Token::Def
+    } else if tok == "extern" {
+      Token::Extern
+    } else if let Ok(n) = tok.parse::<f64>() {
+      Token::Number(n)
+    } else {
+      let first_char = tok.chars().next().unwrap();
+      if token_char(first_char) {
+        Token::Identifier(tok.to_string())
+      } else {
+        Token::Symbol(first_char)
+      }
+    }
+  }
+));
+
+named!(pub lex< &str, Vec<Token> >, many0!(delimited!(
+  many0!(white_space),
+  token,
+  many0!(white_space)
+)));
--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,8 @@ extern crate llvm;
 #[macro_use]
 extern crate nom;
 
-#[derive(Debug)]
-enum Token {
-  Def,
-  Extern,
-  Identifier(String),
-  Symbol(char),
-  Number(f64)
-}
-
-fn token_char(chr: char) -> bool {
-  chr.is_alphanumeric() || chr == '.'
-}
-
-fn comment_char(chr: char) -> bool {
-  chr != '\n'
-}
-
-named!(comment<&str, &str>, preceded!(
-  tag_s!("#"),
-  take_while_s!(comment_char)
-));
-
-named!(white_space<&str, &str>, alt!(comment | tag_s!(" ") | tag_s!("\t") | tag_s!("\n")));
-
-named!(token<&str, Token>, chain!(
-  tok: alt!(take_while1_s!(token_char) | take_s!(1)) ,
-  || {
-    println!("{:?}", tok);
-    if tok == "def" {
-      Token::Def
-    } else if tok == "extern" {
-      Token::Extern
-    } else if let Ok(n) = tok.parse::<f64>() {
-      Token::Number(n)
-    } else {
-      let first_char = tok.chars().next().unwrap();
-      if token_char(first_char) {
-        Token::Identifier(tok.to_string())
-      } else {
-        Token::Symbol(first_char)
-      }
-    }
-  }
-));
-
-named!(lex< &str, Vec<Token> >, many0!(delimited!(
-  many0!(white_space),
-  token,
-  many0!(white_space)
-)));
+mod lexer;
 
 fn main() {
-    println!("{:?}", lex(&"hello    \ntest+ing #!(*@)#(*) def \nextern  129382 1.198 198.2e2 #blah"));
+    println!("{:?}", lexer::lex(&"hello    \ntest+ing #!(*@)#(*) def \nextern  129382 1.198 198.2e2 #blah"));
 }
